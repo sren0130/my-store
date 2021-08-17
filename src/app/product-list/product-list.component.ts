001e3e488f252b3c871ea1c3d5f9e93a4c5fc73b
sren0130 <70994978+sren0130@users.noreply.github.com>
2021-08-17 10:27:28 -0400

    Added all missing, make it all

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
+  products = products;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  share() {
+    window.alert('The product has been shared!');
   }
-
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
